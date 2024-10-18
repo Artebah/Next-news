fc5ac761c8403099697005841472271f97b63e7f
Artem Litvin <mafioznik445@gmail.com>
2024-10-18 15:40:12 +0300

    Handle errors

--- app/archive/@archive/[year]/page.tsx
+++
@@ -1,9 +0,0 @@
-import NewsList from "@/components/NewsList";
-import { getNewsForYear } from "@/lib/news.js";
-
-export default function FilteredNewsPage({ params }: { params: { year: string } }) {
-  const newsYear = params.year;
-  const news = getNewsForYear(newsYear);
-
-  return <NewsList news={news} />;
-}
